ef4278ddf83cea803674ac05d7c394310561c5d6
Kauã Ribeiro Mendes <ribeirocyber123@gmail.com>
2026-09-24 20:16:59 -0300

    adicionada mensagem de erro na criação/cadastrado de um filme

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,40 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import { log } from 'console';
 
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(express.json()); 
+
 app.get('/movies', async (_, res) => {
     const movies = await prisma.movie.findMany({
         orderBy: { title: 'asc' },
-        include: { genres: true, languages: true }
+        include: { genres: true, languages: true },
     });
     res.json(movies);
+});
+
+app.post('/movies', async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } =
+        req.body;
+
+    try {
+        await prisma.movie.create({
+            data: {
+                title, 
+                genre_id, 
+                language_id, 
+                oscar_count, 
+                release_date: new Date(release_date), 
+            },
+        });
+    } catch (error) {
+        res.status(500).send({ message: 'Falha ao cadastrar o filme' });
+    }
+
+    res.status(201).send();
 });
 
 app.listen(port, () => {
